447e8d3399cb63ad5ee952fa6e91275880ad2148
jccalhoun <jccalhoun@users.noreply.github.com>
2020-02-01 11:43:01 -0500

    refactored bottom bing links with comments

--- bottomlinksBing.js
+++ bottomlinksBing.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name	bottom links
+// @name	bing bottom links
 // @description	Adds working on adding links to the bottom of bing
-// @version	0.0.1
+// @version	0.0.2
 
 // @include	http://www.bing.com/*
 // @include	https://www.bing.com/*
@@ -9,15 +9,6 @@
 // @grant       none
 
 // ==/UserScript==
-
-//var SEARCH_ON = 'Search on:';
-//var POSITION = 'left';
-
-
-
-//var PLACEHOLDER_SELECTORS = '#resultStats, #b_results, .b-wordstat__text, .searchresults b, #ext_link, #sidebar, .b-global-wrapper, #gs_ab, #left, #zero_click_wrapper, bing-summary, .bing-summary, #bing-summary';
-//var results = document.querySelector(PLACEHOLDER_SELECTORS);
-//var results = document.querySelector("#b_results"); this puts it at the top
 //this should put it at the bottom because it looks for .b_pag on the page. 
 var results = document.querySelector(".b_pag")
 
@@ -26,16 +17,11 @@ var other = document.createElement('div');
 other.setAttribute("id", "altsearch");
 other.setAttribute("style", "width: 1000px; font-size: small; margin: 10px 10px 5px 159px;");
 
-
-// tests if the url does have ?query= if it does then indexOf will return a number higher than -1 because that's what indexOf does. then else if the url has ?q= and then elses to the last case. 
-//so the regex matches anything starting with the character and matches everything until it gets to the & character. I think the query.replace is for one of the ones that has stuff after that the regex doesn't catch? I'm not sure which search engine that is though. 
-//if (window.location.href.indexOf("?query=") > -1) {
-  //  query = window.location.href.match(/\?query=[^&]*/gi)[0].substr(7);
-//} else if (window.location.href.indexOf("?q=") > -1) {
-    query = window.location.href.match(/\?q=[^&]*/gi)[0].substr(3);
-//} else {
-  //  query = window.location.href.match(/\?p=[^&]*/gi)[0].substr(3);}
-//query = query.replace(/\+filterui%3[^&^+]*/gi,"");
+//so the regex matches anything starting with the character and matches everything until it gets to the & character. It fails though sometimes because the first time you search on bing it uses a different url scheme than this
+// if you search from the search box in firefox it gives https://www.bing.com/search?form=MOZSBR&pc=MOZI&q=typing then if you click images and back to search you get this https://www.bing.com/search?q=typing&FORM=HDRSC1
+//query = window.location.href.match(/\?q=[^&]*/gi)[0].substr(3);
+//this is from the other one and seems to work. I don't know if the (?:\?|&) is nescessary. it tells the regex not to catch that. maybe it is for if you search and then research and there are two terms in the url?
+query = window.location.search.match(/(?:\?|&)q=([^&]*)/)[1];
 links = "Try this search on " +
     "<a href =\"https://www.google.com/search?q=" + query + "\">Google</a>, " +
     "<a href =\"http://www.bing.com/search?q=" + query + "\">Bing</a>, " +
@@ -47,13 +33,5 @@ links = "Try this search on " +
     "<a href =\"http://scholar.google.com/scholar?q=" + query + "\">Google Scholar</a>, " +
     "<a href =\"http://academic.research.microsoft.com/Search?query=" + query + "\">MS Academic</a>";
 other.innerHTML = links;
-
-//this puts it at the bottom
-//if (window.location.host == "www.bing.com") {
-//console.log("bing baby!");
-//var bingBottomResults = document.querySelector(".b_pag");
-//var other2 = other.cloneNode(true);
-//bingBottomResults.parentNode.insertBefore(other2, bingBottomResults);
-//}
-
 results.parentNode.insertBefore(other, results);
+// when it doesn't work it gives this error in console TypeError: window.location.href.match(...) is null
