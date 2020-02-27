e2815d6b89b22d4a32f5b5990f070387d7f2d7be
jccalhoun <jccalhoun@users.noreply.github.com>
2020-02-27 16:45:13 -0500

    fixed the styling issue

--- ddgtest.js
+++ ddgtest.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name       ddg dropdown test
 // @namespace   Violentmonkey Scripts
-// @match       https://duckduckgo.com/
-// @grant       none
-// @version     2020.02.26
+
+// @match           *://duckduckgo.com/*
+// @version     2020.02.27
 // @author      -
 // @description 2/26/2020, 10:07:41 PM
 // @grant        GM_addStyle
 // ==/UserScript==
-//so this puts it behind the content and I can't figure out how to fix it. .zcm-wrap position:relative breaks it. changing that to static fixes it. 
-GM_addStyle (".sDropbtn {border: none; cursor: pointer; background-color: #fafafa;}" +
+//so this puts it behind the content and I can't figure out how to fix it. .zcm-wrap position:relative breaks it. changing that to static fixes it. the font-famil:inherit makes it match the style
+GM_addStyle (".sDropbtn {border: none; cursor: pointer; background-color: #fafafa; font-family: inherit;}" +
             ".sDropdown {display: inline-block;}" +
     ".sDropdown-content {display: none; position: absolute; background-color: #fff !important; z-index: 1;}" +
    ".sDropdown-content a {color: 666; padding: 12px 16px !important; text-decoration: none; display: block;}" +
@@ -27,7 +27,7 @@ GM_addStyle (".sDropbtn {border: none; cursor: pointer; background-color: #fafaf
 
 
 //so i need to put ddgInsert into the wait function
-//based on https://stackoverflow.com/questions/34863788/how-to-check-if-an-element-has-been-loaded-on-a-page-before-running-a-script 
+//based on https://stackoverflow.com/questions/34863788/how-to-check-if-an-element-has-been-loaded-on-a-page-before-running-a-script
 var counter = 0;
 console.log(document.getElementById("duckbar_dropdowns"));
 function waitForElement(id, callback){
@@ -45,7 +45,7 @@ function waitForElement(id, callback){
 
 waitForElement("duckbar_dropdowns", function(){
 //    alert("element is loaded.. do stuff");
-  
+
 
   //drop down stuff
 var ddgResults = document.getElementById('duckbar_dropdowns');
@@ -69,15 +69,15 @@ var addSearchElement = 'searchListener';
     var scholarLink = "<a href =\"http://scholar.google.com/scholar?q=" + ddgSearchTerms() + "\">Google Scholar</a>";
     var msAcademicLink = "<a href =\"https://academic.microsoft.com/#/search?iq=" + ddgSearchTerms() + "\">MS Academic</a>";
     var wikipedia = "<a href =\"https://www.bing.com/search?q=site%3Aen.wikipedia.org+" + ddgSearchTerms() + "\">Wikipedia</a>";
-	
-	
+
+
 	//on bing I am putting it inside a ul so i create a li. here there is no ul so if i use li it creates it with a bullet point. but if i change it to ul it moves. so maybe leave it at li with list-style-type: none
 var ddgInsert = function() {
     console.log(ddgSearchTerms());
     var newItem = document.createElement("li");
-  
+
 newItem.id = addSearchElement;
-links = `<div class="sDropdown">
+var links = `<div class="sDropdown">
 <button class="sDropbtn zcm__link  js-zci-link">Alt Search</button>
 <div class="sDropdown-content">`+ googleLink + bingLink + yahooLink + swagLink + duckLink + wolfLink + twitterLink + scholarLink + msAcademicLink + wikipedia +`</div></div>`;
 newItem.innerHTML = links;
