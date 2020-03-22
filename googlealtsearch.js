7de98eb9970dc6e71cc693455facab0e973fc9f3
jccalhoun <jccalhoun@users.noreply.github.com>
2020-03-22 11:44:44 -0400

    commented out a test for the url and moved links out scholarBeforeMore to make like other scripts

--- googlealtsearch.js
+++ googlealtsearch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         google alt search
 // @namespace    http://tampermonkey.net/
-// @version      2020-03-20
+// @version      2020-03-22
 // @description  Add alt search to google
 // @author       You
 // @include	http://www.google.com*
@@ -23,21 +23,13 @@ GM_addStyle (".dropbtn {border: none; cursor: pointer; background-color: #ffffff
 var scholarEleId = 'hdtb-us-scholar'; //when I genericize this I will need to make this a generic lable
     var results = function() {
         var result = window.location.search.match(/(?:\?|&)q=([^&]*)/)[1];
-    if (window.location.href.indexOf('#q=') > -1) {
-        result =  window.location.href.match(/#q=[^&]*/gi)[0].substr(3);}
+  //  if (window.location.href.indexOf('#q=') > -1) {
+  //      result =  window.location.href.match(/#q=[^&]*/gi)[0].substr(3);}
     return result;
     };
 //adds this in so it can check to see if it exists after the page refreshes
-var scholarBeforeMore = function() {
-   
-    var node = document.createElement("div");
-    node.id = scholarEleId;
-    node.classList.add('hdtb-mitem');
-    node.classList.add('hdtb-imb');
-    //creates the div
-  
-    document.getElementById("hdtb-msb-vis").appendChild(node);
-    var googleLink = "<a href =\"https://www.google.com/search?q=" + results() + "\">Google</a>";
+
+var googleLink = "<a href =\"https://www.google.com/search?q=" + results() + "\">Google</a>";
     var bingLink = "<a href =\"http://www.bing.com/search?q=" + results() + "\">Bing</a>";
     var yahooLink = "<a href =\"http://search.yahoo.com/search?p=" + results() + "\">Yahoo</a>";
     var swagLink = "<a href =\"http://www.swagbucks.com/?f=51&t=w&p=1&q=" + results() + "\">Swagbucks</a>";
@@ -46,6 +38,17 @@ var scholarBeforeMore = function() {
     var twitterLink = "<a href =\"http://twitter.com/search?q=" + results() + "\">Twitter</a>";
     var scholarLink = "<a href =\"http://scholar.google.com/scholar?q=" + results() + "\">Google Scholar</a>";
     var msAcademicLink = "<a href =\"https://academic.microsoft.com/#/search?iq=" + results() + "\">MS Academic</a>";
+
+var scholarBeforeMore = function() {
+   
+    var node = document.createElement("div");
+    node.id = scholarEleId;
+    node.classList.add('hdtb-mitem');
+   node.classList.add('hdtb-imb');
+    //creates the div
+  
+    document.getElementById("hdtb-msb-vis").appendChild(node);
+    
     //this next part is the dropdown from https://www.w3schools.com/howto/howto_css_dropdown.asp
     document.getElementById('hdtb-us-scholar').insertAdjacentHTML('afterbegin', `<div class="dropdown">
 <button class="dropbtn"><img alt="" width="16" height="16"src="data:image/svg+xml;base64,PHN2ZyBmb2N1c2FibGU9ImZhbHNlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZD0iTTE1LjUgMTRoLS43OWwtLjI4LS4yN0E2LjQ3MSA2LjQ3MSAwIDAgMCAxNiA5LjUgNi41IDYuNSAwIDEgMCA5LjUgMTZjMS42MSAwIDMuMDktLjU5IDQuMjMtMS41N2wuMjcuMjh2Ljc5bDUgNC45OUwyMC40OSAxOWwtNC45OS01em0tNiAwQzcuMDEgMTQgNSAxMS45OSA1IDkuNVM3LjAxIDUgOS41IDUgMTQgNy4wMSAxNCA5LjUgMTEuOTkgMTQgOS41IDE0eiIvPjwvc3ZnPg==" /> Alt Search</button>
