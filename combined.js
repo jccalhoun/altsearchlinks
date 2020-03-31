803107650d48d5db4e69ddfae0a0e706f5ff1471
jccalhoun <jccalhoun@users.noreply.github.com>
2020-03-31 13:30:30 -0400

    reworked results function

--- combined.js
+++ combined.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         combined
 // @namespace    http://tampermonkey.net/
-// @version      2020.03.30
+// @version      2020.03.31
 // @description  altsearchrefactored
 // @author       You
 // @include	http://www.bing.com/*
@@ -36,15 +36,22 @@ function addSiteStyle(css) {
 addSiteStyle(".dropbtn {background-color: #fff !important; font-family: Arial, Helvetica, sans-serif; color: #444444; font-size: 11px !important; line-height: 30px !important;}"+
     ".dropdown:hover .dropbtn {background-color: #3e8e41;}");
 
+/* I will probably need to use something like the original alt search to get it to find the correct selector: //placeholder selector lists the different elements that the different sites have that i want to put stuff after. then the document.querySelector cycles through them until it fins the first match https://www.w3schools.com/jsref/met_document_queryselector.asp
+
+var PLACEHOLDER_SELECTORS = '#resultStats, #b_results, .b-wordstat__text, .searchresults b, #ext_link, #sidebar, .b-global-wrapper, #gs_ab, #left, #zero_click_wrapper, bing-summary, .bing-summary, #bing-summary, #ucs';
+var results = document.querySelector(PLACEHOLDER_SELECTORS); */
+
+
 var addSearchElement = 'searchListener';
 
-var results = function () {
+var results = window.location.search.match(/(?:\?|&)q=([^&]*)/)[1];
+    //function () {
     //the match looks for anything after q= and it returns an array so we need the [1]. i am not sure what the (?:\?|&) is for. it tells it not to match that 
-    var result = window.location.search.match(/(?:\?|&)q=([^&]*)/)[1];
+    //var result = window.location.search.match(/(?:\?|&)q=([^&]*)/)[1];
     //if (window.location.href.indexOf('#q=') > -1) {
     //  result =  window.location.href.match(/#q=[^&]*/gi)[0].substr(3);}
-    return result;
-};
+   //return result;
+//};
 
 
 //the first var results finds the part of the search url after q= I don't know that I need the second one. it finds the thing after #q but I don't know which search engines use that? I think that if I just set results = window.location.search.match(/(?:\?|&)q=([^&]*)/)[1]; then I need to make sure I call "results" NOT results() 
@@ -54,16 +61,16 @@ var results = function () {
 //so all these are in a function currently but I think it should be fine to pull them out since that is how it is in the old alt search but the old one doesn't declare individual variables. Doing it individually could allow for not putting a link to the site you are already on (so a link to bing wouldn't show up if you were already on bing)
 
 
-var googleLink = '<a href =\"https://www.google.com/search?q=' + results() + '\">Google</a>';
-var bingLink = "<a href =\"http://www.bing.com/search?q=" + results() + "\">Bing</a>";
-var yahooLink = "<a href =\"http://search.yahoo.com/search?p=" + results() + "\">Yahoo</a>";
-var swagLink = "<a href =\"http://www.swagbucks.com/?f=51&t=w&p=1&q=" + results() + "\">Swagbucks</a>";
-var duckLink = "<a href =\"https://duckduckgo.com/?q=" + results() + "\">DuckDuckGo</a>";
-var wolfLink = "<a href =\"http://www.wolframalpha.com/input/?i=" + results() + "\">WolframAlpha</a>";
-var twitterLink = "<a href =\"http://twitter.com/search?q=" + results() + "\">Twitter</a>";
-var scholarLink = "<a href =\"http://scholar.google.com/scholar?q=" + results() + "\">Google Scholar</a>";
-var msAcademicLink = "<a href =\"https://academic.microsoft.com/search?q=" + results() + "\">MS Academic</a>";
-var wikipedia = "<a href =\"https://www.bing.com/search?q=site%3Aen.wikipedia.org+" + results() + "\">Wikipedia</a>";
+var googleLink = '<a href =\"https://www.google.com/search?q=' + results + '\">Google</a>';
+var bingLink = "<a href =\"http://www.bing.com/search?q=" + results + "\">Bing</a>";
+var yahooLink = "<a href =\"http://search.yahoo.com/search?p=" + results + "\">Yahoo</a>";
+var swagLink = "<a href =\"http://www.swagbucks.com/?f=51&t=w&p=1&q=" + results + "\">Swagbucks</a>";
+var duckLink = "<a href =\"https://duckduckgo.com/?q=" + results + "\">DuckDuckGo</a>";
+var wolfLink = "<a href =\"http://www.wolframalpha.com/input/?i=" + results + "\">WolframAlpha</a>";
+var twitterLink = "<a href =\"http://twitter.com/search?q=" + results + "\">Twitter</a>";
+var scholarLink = "<a href =\"http://scholar.google.com/scholar?q=" + results + "\">Google Scholar</a>";
+var msAcademicLink = "<a href =\"https://academic.microsoft.com/search?q=" + results + "\">MS Academic</a>";
+var wikipedia = "<a href =\"https://www.bing.com/search?q=site%3Aen.wikipedia.org+" + results + "\">Wikipedia</a>";
 
 
 
